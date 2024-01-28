5be0ff42096c032b6170594059398aa1d1a357be
Basically_Empty <rom.van.din@gmail.com>
2024-01-28 23:39:33 +0700

    Рефакторинг

--- src/components/modal.js
+++ src/components/modal.js
@@ -24,19 +24,4 @@ const closePopup = el => {
   document.removeEventListener('mousedown', clickHandler);
 }
 
-const showImg = evt => {
-  const card = evt.target.closest('.card');
-  const cardImg = card.querySelector('.card__image');
-  const cardTitle = card.querySelector('.card__title');
-  const popupTypeImage = document.querySelector('.popup_type_image');
-  const popupImage = document.querySelector('.popup__image');
-  const popupCaption = document.querySelector('.popup__caption');
-
-  popupImage.src = cardImg.src;
-  popupImage.alt = cardTitle.textContent;
-  popupCaption.textContent = cardTitle.textContent;
-
-  openPopup(popupTypeImage);
-}
-
-export { openPopup, closePopup, showImg }
+export { openPopup, closePopup }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import '../src/index.css';
 import { initialCards } from './components/cards.js';
 import { createCard, addCard, deleteCard, likeCard } from './components/card.js';
-import { openPopup, closePopup, showImg } from './components/modal.js';
+import { openPopup, closePopup } from './components/modal.js';
 
 const editBtn = document.querySelector('.profile__edit-button');
 const addBtn = document.querySelector('.profile__add-button');
 const closePopupBtns = document.querySelectorAll('.popup__close');
 const popupTypeEdit = document.querySelector('.popup_type_edit');
 const popupTypeNew = document.querySelector('.popup_type_new-card');
+const popupTypeImage = document.querySelector('.popup_type_image');
+const popupImage = document.querySelector('.popup__image');
+const popupCaption = document.querySelector('.popup__caption');
 const profileName = document.querySelector('.profile__title');
 const profileDescription = document.querySelector('.profile__description');
 const formEdit = document.forms['edit-profile'];
@@ -16,13 +19,6 @@ const descriptionInput = formEdit.elements.description;
 const formNew = document.forms['new-place'];
 const placeName = formNew.elements['place-name'];
 const placeLink = formNew.elements.link;
-
-const initializeCards = arr => {
-  arr.forEach(el => {
-    addCard((createCard(el.name, el.link, deleteCard, likeCard, showImg)))
-  })
-}
-
 
 editBtn.addEventListener('click', () => {
   openPopup(popupTypeEdit);
@@ -48,11 +44,29 @@ const formEditSubmitHandler = evt => {
   closePopup(popupTypeEdit);
 }
 
+const showImg = evt => {
+  const card = evt.target.closest('.card');
+  const cardImg = card.querySelector('.card__image');
+  const cardTitle = card.querySelector('.card__title');
+
+  popupImage.src = cardImg.src;
+  popupImage.alt = cardTitle.textContent;
+  popupCaption.textContent = cardTitle.textContent;
+
+  openPopup(popupTypeImage);
+}
+
 const formNewSubmitHandler = evt => {
   evt.preventDefault();
   addCard(createCard(placeName.value, placeLink.value, deleteCard, likeCard, showImg));
   closePopup(popupTypeNew);
   formNew.reset();
+}
+
+const initializeCards = arr => {
+  arr.forEach(el => {
+    addCard((createCard(el.name, el.link, deleteCard, likeCard, showImg)))
+  })
 }
 
 formEdit.addEventListener('submit', formEditSubmitHandler)
